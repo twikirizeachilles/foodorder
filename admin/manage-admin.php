3680744e94647907ff801b4c8131ffc7cb1f9b9b
twikirizeachilles <twisarch20@gmail.com>
2025-11-27 00:37:40 +0300

    added the partials ,manage- order , food , category

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,23 +1,5 @@
-<html>
-<head>
-    <title>Food Order Website - Home Page </title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-<body>
-    <!-- Menu Section Starts -->
-     <div class="menu text-center">
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="manage-admin.php">Admin</a></li>
-                <li><a href="manage-category.php">Category</a></li>
-                <li><a href="manage-food.php">Food</a></li>
-                <li><a href="manage-order.php">Order</a></li>
-            </ul>
-        </div> 
-     </div>
-    <!-- Menu Section Ends -->
- 
+<?php include('partials/menu.php'); ?>
+
     <!-- Main Content Section Starts -->
      <div class="main-content">
         <div class="wrapper">
@@ -25,12 +7,4 @@
      </div>
     <!-- Main Content Section Ends -->
 
-    <!-- Footer Section Starts -->
-     <div class="footer">
-        <div class="wrapper">
-            <p class="text-center">All rights reserved. Designed By <a href="#">Twikirize Achilles</a></p>
-        </div>
-     </div>
-    <!-- Footer Section Ends -->
-</body>
-</html>
+<?php include('partials/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer Section Starts -->
+     <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">All rights reserved. Designed By <a href="#">Twikirize Achilles</a></p>
+        </div>
+     </div>
+    <!-- Footer Section Ends -->
+</body>
+</html>
