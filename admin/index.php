45b9b3ceed4e017a1be423fa0d87ce83f7c9c99c
twikirizeachilles <twisarch20@gmail.com>
2025-11-26 23:53:39 +0300

    added the manage-admin.php

---
+++ admin/index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <title>Food Order Website - Home Page </title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu Section Starts -->
+     <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+            </ul>
+        </div> 
+     </div>
+    <!-- Menu Section Ends -->
+ 
+    <!-- Main Content Section Starts -->
+     <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashboard </h1>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                categories
+            </div>
+            <div class="clearfix"></div>
+        </div>  
+     </div>
+    <!-- Main Content Section Ends -->
+
+    <!-- Footer Section Starts -->
+     <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">All rights reserved. Designed By <a href="#">Twikirize Achilles</a></p>
+        </div>
+     </div>
+    <!-- Footer Section Ends -->
+</body>
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Food Order Website - Home Page </title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu Section Starts -->
+     <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+            </ul>
+        </div> 
+     </div>
+    <!-- Menu Section Ends -->
+ 
+    <!-- Main Content Section Starts -->
+     <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin </h1>
+     </div>
+    <!-- Main Content Section Ends -->
+
+    <!-- Footer Section Starts -->
+     <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">All rights reserved. Designed By <a href="#">Twikirize Achilles</a></p>
+        </div>
+     </div>
+    <!-- Footer Section Ends -->
+</body>
+</html>
