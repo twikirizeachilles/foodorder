1c48d7320d8d4bd18ea91917d8cec70473a0fc97
twikirizeachilles <twisarch20@gmail.com>
2025-11-27 12:15:47 +0300

    added tables to all the manage files

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -3,6 +3,29 @@
     <div class="main-content">
         <div class="wrapper">
             <h1>Manage Order</h1>
+            <br><br>
+
+            <a href="#" class="btn-primary">Add Admin</a>
+            <br><br><br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td>1.</td>
+                    <td>Twis Achilles</td>
+                    <td>twisarch</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+            </table>
             
     </div>
 <?php include('partials/footer.php'); ?>
