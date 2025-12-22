db92fa66998b843ebb05bed4f938c576e025d432
twikirizeachilles <twisarch20@gmail.com>
2025-12-22 10:10:35 +0300

    add food.php without proper functionality

---
+++ admin/add-food.php
@@ -0,0 +1,72 @@
+<?php include ('partials/menu.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Food</h1>
+        <br><br>
+        <form action="" method="post" enctype="multipart/form-data">
+            <table class="tbl-30">
+                <tr>
+                    <td>Title:</td>
+                    <td>
+                        <input type="text" name="title" placeholder="Title of the food">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Description:</td>
+                    <td>
+                        <textarea name="description" cols="30" rows="5" placeholder="Description of the food"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td>
+                        <input type="number" name="price">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Select Image:</td>
+                    <td>
+                        <input type="file" name="image">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Category:</td>
+                    <td>
+                        <select name="category">
+                            <option value="1">Food</option>
+                            <option value="2">Snack</option>
+                            <option value="3">Drinks</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Featured:</td>
+                    <td>
+                        <input type="radio" name="featured" value="Yes"> Yes
+                        <input type="radio" name="featured" value="No"> No
+                    </td>
+                </tr>
+                <tr>
+                    <td>Active:</td>
+                    <td>
+                        <input type="radio" name="active" value="Yes"> Yes
+                        <input type="radio" name="active" value="No"> No
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Food" class="btn-secondary">
+                    </td>
+                </tr>
+
+            </table>
+
+        </form>
+
+    </div>
+</div>
+
+<?php include('partials/footer.php'); ?>
