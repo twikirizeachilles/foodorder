caf1a5a7581250a6e9135f90a516c37694cf6fbc
twikirizeachilles <twisarch20@gmail.com>
2025-12-22 21:43:57 +0300

    added functinoality to add food.php and ensured that is possible to connect to database

--- admin/add-food.php
+++ admin/add-food.php
@@ -4,6 +4,13 @@
     <div class="wrapper">
         <h1>Add Food</h1>
         <br><br>
+        <?php 
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+        ?>
         <form action="" method="post" enctype="multipart/form-data">
             <table class="tbl-30">
                 <tr>
@@ -36,9 +43,39 @@
                     <td>Category:</td>
                     <td>
                         <select name="category">
-                            <option value="1">Food</option>
-                            <option value="2">Snack</option>
-                            <option value="3">Drinks</option>
+                            <?php 
+                                //create php code to display categories from database
+                                //create sql to get all active categories from database
+                                $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
+                                //execute query
+                                $res = mysqli_query($conn, $sql);
+                                //count rows to check whether we have categories or not
+                                $count = mysqli_num_rows($res);
+                                //if count is greater than 0, we have categories else we don't have categories
+                                if($count>0)
+                                {
+                                    //we have categories
+                                    while($row=mysqli_fetch_assoc($res))
+                                    {
+                                        //get the details of categories
+                                        $id = $row['id'];
+                                        $title = $row['title'];
+                                        ?>
+                                        <option value="<?php echo $id; ?>"><?php echo $title; ?></option>
+                                        <?php
+
+                                    }
+                                }
+                                else
+                                {
+                                    //we don't have categories
+                                    ?>
+                                    <option value="0">No Category Found</option>
+                                    <?php
+                                }
+                                
+                            ?>
+                            
                         </select>
                     </td>
                 </tr>
@@ -65,6 +102,108 @@
             </table>
 
         </form>
+        <?php
+
+        //check whether the submit button is clicked or not
+        if(isset($_POST['submit']))
+        {
+            //get the value from form
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+            $price = $_POST['price'];
+            $category = $_POST['category'];
+
+            //check whether radio button for featured is active or not 
+            if(isset($_POST['featured']))
+            {
+                //get the value from form
+                $featured = $_POST['featured'];
+            }
+            else
+            {
+                $featured = "No";
+            }
+            if(isset($_POST['active']))
+            {
+                //get the value from form
+                $active = $_POST['active'];
+            }
+            else
+            {
+                $active = "No";
+            }
+
+            //image uploaded
+            //check if the image is selected or not
+            if(isset($_FILES['image']['name']))
+            {
+                $image_name = $_FILES['image']['name'];
+                //check wheather the is selected or not and upload image only if selected
+                if($image_name != "")
+                {
+                    //image is selected 
+                    //upload the image
+                    //get the extension of our image (jpg, png, gif, etc) e.g. food1.jpg
+                    $ext = end(explode('.', $image_name));
+
+                    //rename the image
+                    $image_name = "Food_name_".rand(0000, 9999).'.'.$ext; //e.g. Food_name_834.jpg
+                    
+                    // source path and destination path
+                    $src = $_FILES['image']['tmp_name'];
+                    $dst = "../images/food/".$image_name;
+
+                    //finally upload the image
+                    $upload = move_uploaded_file($src, $dst);
+
+                    //check weather image uploaded or not
+                    if($upload==false)
+                    {
+                        //failed to upload image
+                        //redirected to manage food page with error message
+                        $_SESSION['upload'] = "<div class='error'>Failed to Upload Image.</div>";
+                        header('location:'.SITEURL.'admin/manage-food.php');
+                        //stop the process
+                        die();
+                    }
+                }
+            }
+            else
+            {
+                $image_name = "";
+            }
+            //insert into database 
+            $sql2 = "INSERT INTO tbl_food SET
+                title = '$title',
+                description = '$description',
+                price = $price,
+                image_name = '$image_name',
+                category_id = $category,
+                featured = '$featured',
+                active = '$active'
+            ";
+            //execute the query
+            $res2 = mysqli_query($conn, $sql2);
+            //check wheather data inserted or not
+            if($res2==true){
+                //data inserted successfully
+                $_SESSION['add'] = "<div class='success'>Food Added Successfully.</div>";
+                //redirect to manage food page
+                header('location:'.SITEURL.'admin/manage-food.php');
+            }
+            else{
+                //failed to insert data
+                $_SESSION['add'] = "<div class='error'>Failed to Add Food.</div>";
+                //redirect to manage food page
+                header('location:'.SITEURL.'admin/manage-food.php');
+            }
+            
+            //redirect to manage food page
+            header('location:'.SITEURL.'admin/manage-food.php');
+        } 
+
+
+        ?>
 
     </div>
 </div>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -5,26 +5,77 @@
             <h1>Manage Food </h1>
             <br><br>
 
-            <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary ?>" class="btn-primary">Add Food</a>
+            <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
             <br><br><br>
+
+            <?php 
+                if(isset($_SESSION['add'])){
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                }
+            ?>
 
             <table class="tbl-full">
                 <tr>
                     <th>S.N.</th>
-                    <th>Full Name</th>
-                    <th>Username</th>
+                    <th>Title</th>
+                    <th>Price </th>
+                    <th>Image</th>
+                    <th>Featured</th>
+                    <th>Active</th>
                     <th>Actions</th>
                 </tr>
+                <?php
+                    $sql = "SELECT * FROM tbl_food";
+                    //execute the query
+                    $res = mysqli_query($conn, $sql);
+                    //count rows
+                    $count = mysqli_num_rows($res); 
 
-                <tr>
-                    <td>1.</td>
-                    <td>Twis Achilles</td>
-                    <td>twisarch</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+                    //create serial number variable
+                    $sn = 1;
+
+                    if($count>0){
+                        while($row = mysqli_fetch_assoc($res)){
+                            $id = $row['id'];
+                            $title = $row['title'];
+                            $price = $row['price'];
+                            $image_name = $row['image_name'];
+                            $featured = $row['featured'];
+                            $active = $row['active'];
+                            ?>
+                                <tr>
+                                    <td><?php echo $sn++; ?></td>
+                                    <td><?php echo $title; ?>/td>
+                                    <td><?php echo $price; ?></td>
+                                    <td>
+                                        <?php 
+                                            if($image_name==""){
+                                                echo "<div class='error'>Image not Added.</div>";
+                                            }
+                                            else{
+
+                                                ?>
+                                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="100px">
+                                                <?php
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="#" class="btn-secondary">Update Food</a>
+                                        <a href="#" class="btn-danger">Delete Food</a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    }
+                    else{
+                        echo "<tr><td colspan='7' class='error'>Food not Added Yet.</td></tr>";
+                    }
+                ?>
+                
             </table>
             
     </div>
